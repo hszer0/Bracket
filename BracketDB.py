fa8ad9e0add54732282b62b0735d0dac890de0ee
hszer0 <hszer0@gmail.com>
2014-07-24 15:20:00 +0200

    Added more tables

--- BracketDB.py
+++ BracketDB.py
@@ -19,3 +19,16 @@ class Database(object):
 
     def remove_participant(self, name):
         self.c.execute("delete from participants where name = (?)", (name,))
+
+    def add_tournament(self, name, date):
+        self.c.execute("insert into tournaments (name, date) values (?, ?)", (name, date))
+        return self.c.lastrowid
+
+    def remove_tournament(self, tid):
+        self.c.execute("delete from tournaments where id = ?", (tid,))
+
+    def add_contender(self, tid, name):
+        self.c.execute("insert into contenders (tid, name) values (?, ?)", (tid, name))
+
+    def remove_contender(self, tid, name):
+        self.c.execute("delete from contenders where tid = ? and name = ?", (tid, name))
--- BracketTests.py
+++ BracketTests.py
@@ -9,16 +9,14 @@ class BracketTests(unittest.TestCase):
     def setUp(self):
         self.db = Database()
 
-    def test_db_connection(self):
-        self.db.c.execute("select name from participants")
-
-    def test_tables(self):
-        self.db.c.execute("select name from participants")
-
-    def test_close_db(self):
+    def tearDown(self):
         self.db.close_connection()
-        with self.assertRaises(Exception):
-            self.db.c.execute("select name from participants")
+
+    def test_tables_and_columns(self):
+        self.db.c.execute("select name from participants")
+        self.db.c.execute("select id, name, date from tournaments")
+        self.db.c.execute("select tid, name from contenders")
+        self.db.c.execute("select tid, round, name1, name2, score1, score2 from scores")
 
     def test_add_and_remove_participant(self):
         #first clear the table
@@ -32,6 +30,39 @@ class BracketTests(unittest.TestCase):
 
         #test for removing
         self.db.remove_participant("Patrick")
-        self.db.c.execute("select name from participants where name = 'Patrick'")
+        self.db.c.execute("select name from participants")
         self.assertIsNone(self.db.c.fetchone())
+
+    def test_add_and_remove_tournament(self):
+        #first clear the table
+        self.db.c.execute("delete from tournaments")
+
+        #test for adding
+        tid = self.db.add_tournament("UT2k4", "2014-01-01")
+        self.db.c.execute("select name, date from tournaments")
+        row = self.db.c.fetchone()
+        self.assertEqual(("UT2k4", "2014-01-01"), row)
+
+        #test for removing
+        self.db.remove_tournament(tid)
+        self.db.c.execute("select id from tournaments")
+        self.assertIsNone(self.db.c.fetchone())
+
+    def test_add_and_remove_contender(self):
+        #first clear the table
+        self.db.c.execute("delete from contenders")
+
+        #test for adding
+        tid = self.db.add_tournament("UT2k4", "2014-01-01")
+        self.db.add_contender(tid, "Patrick")
+        self.db.c.execute("select tid, name from contenders")
+        row = self.db.c.fetchone()
+        self.assertEqual((tid, "Patrick"), row)
+
+        #test for removing
+        self.db.remove_contender(tid, "Patrick")
+        self.db.remove_tournament(tid)
+        self.db.c.execute("select * from contenders")
+        self.assertIsNone(self.db.c.fetchone())
+
 
